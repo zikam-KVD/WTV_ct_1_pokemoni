f7957f42cfb29b89634b0652fb42cf5ce6317445
Mirek Zíka <zikam@kvd.zcu.cz>
2024-12-05 10:56:16 +0100

    navigace

---
+++ resources/views/layouts/includes/navigace.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <div class="nav-inner">
+        <a href="{{ route('login') }}">
+            Přihlášení
+        </a>
+        <a href="{{ route('register') }}">
+            Registrace
+        </a>
+    </div>
+</nav>
